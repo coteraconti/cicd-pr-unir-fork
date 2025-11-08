5883e11a8e84dc65f5303ecbdf2fdd7158516963
Roberto Cotera <coteraconti@gmail.com>
2025-11-07 18:31:44 -0600

    lista de palabras en mayusculas

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ def sort_list(items, ascending=True):
 def remove_duplicates_from_list(items):
     return list(set(items))
 
+def to_uppercase(items):
+    return [word.upper() for word in items]
 
 if __name__ == "__main__":
     filename = DEFAULT_FILENAME
@@ -42,6 +44,7 @@ if __name__ == "__main__":
     else:
         print(f"El fichero {filename} no existe")
         word_list = ["ravenclaw", "gryffindor", "slytherin", "hufflepuff"]
+    print(to_uppercase(sort_list(word_list)))
 
     if remove_duplicates:
         word_list = remove_duplicates_from_list(word_list)
